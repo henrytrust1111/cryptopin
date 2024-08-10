3a4255a0bbab6d6a688656d748d78b822966eb52
Ekele1 <jerryoilfarmltd01@gmail.com>
2024-08-10 07:29:18 +0100

    fff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Heropage from '@/components/heropage'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Heropage />
+    </div>
   )
 }
 
