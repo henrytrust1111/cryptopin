46e441b451e4578e22e2e2246f98197f5c1d7722
Ekele1 <jerryoilfarmltd01@gmail.com>
2024-08-10 11:32:02 +0100

    hh

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { CgProfile } from "react-icons/cg";
+import { useRouter } from "next/router";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useEffect } from "react";
 import Link from "next/link";
 
 const Header = () => {
@@ -10,14 +13,17 @@ const Header = () => {
           <div className="w-[15%] h-[80%]">
             <img src="./Logo.svg" alt="" />
           </div>
-          <div className="w-[50%] h-[80%] flex items-center justify-around">
-            <div className="cursor-pointer">Home</div>
-            <div className="cursor-pointer">About</div>
-            <div className="cursor-pointer">Pages</div>
-            <div className="cursor-pointer">Pricing</div>
-            <div className="cursor-pointer">Contact</div>
+          <div className="w-[50%] h-[80%] hidden lg:flex items-center justify-around ">
+            <Link href='/' className="cursor-pointer">Home</Link>
+            <Link href='/about' className="cursor-pointer">About</Link>
+            <Link href='/pages' className="cursor-pointer">Pages</Link>
+            <Link href='/pricing' className="cursor-pointer">Pricing</Link>
+            <Link href='/contact' className="cursor-pointer">Contact</Link>
             <button className="w-[20%] h-[70%] rounded-3xl text-white bg-gradient-to-r from-[#FF5196] via-[#7782FF] to-[#25F6FF]">Login</button>
           </div>
+          <div className="hidden max-[1024px]:flex">
+              <RxHamburgerMenu />
+            </div>
         </div>
       </div>
     </>
